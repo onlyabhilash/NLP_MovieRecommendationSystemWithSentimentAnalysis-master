99224970e399f9225d8565dc29662519e5b5b1f9
onlyabhilash <77659887+onlyabhilash@users.noreply.github.com>
2021-02-23 08:54:35 +0530

    Api-key

--- static/recommend.js
+++ static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-    var my_api_key = 'YOUR_API_KEY';
+    var my_api_key = '35356668f1a04f77582dc98f7b38fe21';
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
-  var my_api_key = 'YOUR_API_KEY';
+  var my_api_key = '35356668f1a04f77582dc98f7b38fe21';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
